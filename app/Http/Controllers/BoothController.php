2996b844a1f8d3da14c17b5bf296c2595a0e5071
KID <jyhsu2000@yahoo.com.tw>
2016-08-09 02:15:28 +0800

    Remove blank lines

--- app/Http/Controllers/BoothController.php
+++ app/Http/Controllers/BoothController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Booth;
 use Illuminate\Http\Request;
 
-
 class BoothController extends Controller
 {
     /**
--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Type;
 use Illuminate\Http\Request;
 
-
 class TypeController extends Controller
 {
     /**

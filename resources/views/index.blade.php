441f08fcf449520f7eeac9ee8ed0cb08a07a66a1
KID <jyhsu2000@yahoo.com.tw>
2016-08-09 00:15:48 +0800

    Update index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,29 +4,26 @@
 
 @section('css')
     <style>
-        div.fullscreen {
-            height: 100vh;
-        }
-        div.fullpage.background {
-            background: url("{{ asset('img/background/index.jpg') }}") no-repeat fixed center;
-        }
-        div.translucent {
-            background-color: rgba(0, 0, 0, 0.6) !important;
+        .jumbotron {
+            text-align: center;
+            background: rgba(100, 100, 100, .6);
+            margin-top: 20vh;
+            padding-top: 40px;
+            border-radius: 20px;
         }
     </style>
 @endsection
-
 @section('content')
-    <div class="fullpage background">
-        <div class="ui inverted translucent vertical center aligned fullscreen segment">
-            <h1 class="ui inverted center aligned icon header" style="margin-top: 20vh;">
+    <div class="jumbotron">
+        <div class="ui translucent vertical center aligned segment">
+            <h1 class="ui inverted center aligned icon header">
                 <i class="circular inverted red massive marker icon"></i>
-                逢甲大學學生社團博覽會
+                逢甲大學 學生社團博覽會
                 <p class="ui sub header">
                     打卡集點抽獎
                 </p>
             </h1>
-            <a href="javascript:void(0)" class="ui large inverted red center aligned button" style="margin-top: 10vh;">GO!</a>
+            <a href="javascript:void(0)" class="ui large inverted red center aligned button" style="margin-top: 5vh;">GO!</a>
         </div>
     </div>
 @endsection

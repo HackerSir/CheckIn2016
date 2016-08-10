a2d59b8f34c475624a2adca038c9004a40000869
Vongola <vongola12324@coder.tw>
2016-08-11 00:01:21 +0800

    Update inti script and composer install cmd.
    Important: init.bat or Win10 might have some bug.

--- init.bat
+++ init.bat
@@ -4,28 +4,22 @@ echo "You MUST set php, composer, npm in your path!"
 pause
 
 rem Deploy Laravel
-echo "Task<1>: Deploying Laravel environment..."
+echo "Task[1]: Deploying Laravel environment..."
 composer install
-copy .env.example .env
-php artisan key:generate
-composer run-script post-update-cmd
-echo "Task<1>: Done."
 
 rem Build Semantic UI
-echo "Task<2>: Building Semantic UI..."
+echo "Task[2]: Building Semantic UI..."
 npm install
 cd semantic
 gulp build
-echo "Task<2>: Done"
 
 rem Remove Extra File
-echo "Task<3>: Removing extra files..."
+echo "Task[3]: Removing extra files..."
 cd ..
 rmdir /S /Q public\semantic\components
 rmdir /S /Q public\semantic\themes\basic
 rmdir /S /Q public\semantic\themes\github
 del /Q public\semantic\semantic.css
 del /Q public\semantic\semantic.js
-echo "Task<3>: Done"
 
 pause

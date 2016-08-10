87a695b2999288a6c048bf58f0591f1634449c17
Vongola <vongola12324@coder.tw>
2016-08-11 00:03:33 +0800

    Update deploy scripts

--- init.bat
+++ init.bat
@@ -6,13 +6,13 @@ pause
 rem Deploy Laravel
 echo "Task[1]: Deploying Laravel environment..."
 composer install
+php artisan migrate
 
 rem Build Semantic UI
 echo "Task[2]: Building Semantic UI..."
 npm install
 cd semantic
 gulp build
-
 rem Remove Extra File
 echo "Task[3]: Removing extra files..."
 cd ..

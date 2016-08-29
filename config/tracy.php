e247af4424db60a8cd63f8a74e1f2adc768e23e6
KID <jyhsu2000@yahoo.com.tw>
2016-08-29 20:10:53 +0800

    Update tracy config

--- config/tracy.php
+++ config/tracy.php
@@ -1,18 +1,19 @@
 <?php
 
 return [
-    'enabled'      => env('APP_DEBUG', false),
-    'showBar'      => true,
-    'accepts'      => [
+    'enabled'       => env('APP_DEBUG', false),
+    'showBar'       => true,
+    'accepts'       => [
         'text/html',
     ],
-    'editor'       => 'subl://open?url=file://%file&line=%line',
-    'maxDepth'     => 4,
-    'maxLength'    => 1000,
-    'scream'       => true,
-    'showLocation' => true,
-    'strictMode'   => true,
-    'panels'       => [
+    'editor'        => 'subl://open?url=file://%file&line=%line',
+    'maxDepth'      => 4,
+    'maxLength'     => 1000,
+    'scream'        => true,
+    'showLocation'  => true,
+    'strictMode'    => true,
+    'editorMapping' => [],
+    'panels'        => [
         'routing'        => true,
         'database'       => true,
         'view'           => true,

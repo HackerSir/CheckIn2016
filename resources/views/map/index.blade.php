46e6d4784842e2d5cbc5b01219c671e104319ca4
KID <jyhsu2000@yahoo.com.tw>
2016-08-30 17:58:54 +0800

    Update map page

--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -6,5 +6,29 @@
     <h2 class="ui teal header center aligned">
         地圖
     </h2>
-        <img src="{{ asset('img/map/map.jpg') }}" alt="地圖" class="ui image">
+    <div>
+        <button class="ui red inverted labeled icon button" id="zoom-btn">
+            <i class="zoom icon"></i> 放大
+        </button>
+        <br/>
+        <a href="{{ asset('img/map/map.jpg') }}" class="ui blue inverted icon button" target="_blank">
+            <i class="external icon"></i> 新頁面開啟
+        </a>
+        <a href="{{ asset('img/map/map.jpg') }}" class="ui brown inverted icon button" download="map">
+            <i class="download icon"></i> 下載
+        </a>
+    </div>
+    <div style="overflow: auto">
+        <img src="{{ asset('img/map/map.jpg') }}" alt="地圖" class="ui image" id="map">
+    </div>
+@endsection
+
+@section('js')
+    <script>
+        $(function () {
+            $('#zoom-btn').click(function () {
+                $('#map').toggleClass('image');
+            });
+        });
+    </script>
 @endsection

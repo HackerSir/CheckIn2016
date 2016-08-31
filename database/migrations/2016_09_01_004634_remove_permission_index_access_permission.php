f5afcb9d1c52c79e0d11e7d7e46ccd219720f1fd
KID <jyhsu2000@yahoo.com.tw>
2016-09-01 00:48:13 +0800

    Remove permission index access permission

---
+++ database/migrations/2016_09_01_004634_remove_permission_index_access_permission.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Permission;
+use App\Role;
+use Illuminate\Database\Migrations\Migration;
+
+class RemovePermissionIndexAccessPermission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Permission::where('name', 'permission.index.access')->delete();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $permPermissionIndexAccess = Permission::create([
+            'name'         => 'permission.index.access',
+            'display_name' => '進入權限面板',
+            'description'  => '進入權限面板，查看各角色權限清單',
+        ]);
+
+        /* @var Role $admin */
+        $admin = Role::where('name', 'Admin')->first();
+        $admin->attachPermission($permPermissionIndexAccess);
+    }
+}

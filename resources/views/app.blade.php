eb6fd1fc431c4977d33598d72c1f166736332b65
KID <jyhsu2000@yahoo.com.tw>
2016-09-04 13:38:00 +0800

    Update favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <link rel="icon" type="image/ico" href="favicon.ico">
+
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="icon" type="image/ico" href="{{ asset('favicon.ico') }}">
 
     <title>@yield('title') - {{ config('site.name') }}</title>
 

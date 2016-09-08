4a5ef3c186c97fa8f30c7471d4039855f0ac3a62
KID <jyhsu2000@yahoo.com.tw>
2016-09-08 18:26:59 +0800

    Add TODO and FIXME

--- app/Http/Controllers/LeaderBoardController.php
+++ app/Http/Controllers/LeaderBoardController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Booth;
 
-
 class LeaderBoardController extends Controller
 {
     public function index()
     {
+        //FIXME: 同一學生重複打卡不該重複計算
+        //TODO: 只計算有投票資格的打卡
         $booths = Booth::with('type', 'points')->get()
             ->sortBy(function ($booth) {
                 return $booth->points->count();

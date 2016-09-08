8ec4c6ca66875c77e879b7b2b0152d20b1a15e89
KID <jyhsu2000@yahoo.com.tw>
2016-09-09 01:03:46 +0800

    Update leader board

--- resources/views/leaderBoard/index.blade.php
+++ resources/views/leaderBoard/index.blade.php
@@ -6,13 +6,19 @@
     <h2 class="ui teal header center aligned">
         打卡次數排行榜
     </h2>
+    <div class="ui warning message">
+        <ul class="list">
+            <li>僅計算新生人數</li>
+            <li>同一人在同一攤位打卡，無論次數，僅計一次</li>
+        </ul>
+    </div>
     <table class="ui selectable celled padded unstackable table" id="booth-table">
         <thead>
         <tr>
             <th>名次</th>
             <th>攤位編號</th>
             <th>攤位</th>
-            <th>打卡次數</th>
+            <th>打卡新生數</th>
         </tr>
         </thead>
         <tbody>
